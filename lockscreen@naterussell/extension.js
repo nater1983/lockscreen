c0aed9775bc2ef66ab8215cbebe420a92adff61c
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2023-10-11 03:12:14 +0000

    Update extension.js

--- lockscreen@naterussell/extension.js
+++ lockscreen@naterussell/extension.js
@@ -1,27 +1,9 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
 import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
@@ -32,26 +14,26 @@ let lockScreenButton;
 // LockScreenButton class
 const LockScreenButton = GObject.registerClass(
 class LockScreenButton extends PanelMenu.Button {
-      super._init(0);
+  _init() {
+    super._init(0);
 
-      let label = new St.Label({
-        text: "Lock Screen",
-        y_align: Clutter.ActorAlign.CENTER,
-      });
+    let label = new St.Label({
+      text: "Lock Screen",
+      y_align: Clutter.ActorAlign.CENTER,
+    });
 
-      this.add_child(label);
+    this.add_child(label);
 
-      this.connect('button-press-event', this._lockScreenActivate);
-    }
+    this.connect('button-press-event', this._lockScreenActivate);
+  }
 
-    _lockScreenActivate() {
-      let [success, pid] = Gio.Subprocess.new(['xscreensaver-command', '-lock'], Gio.SubprocessFlags.NONE);
-      if (success) {
-        pid.wait(null);
-      }
+  _lockScreenActivate() {
+    let [success, pid] = Gio.Subprocess.new(['xscreensaver-command', '-lock'], Gio.SubprocessFlags.NONE);
+    if (success) {
+      pid.wait(null);
     }
   }
-);
+});
 
 // Main extension class
 export default class LockScreenPopupExtension extends Extension {
