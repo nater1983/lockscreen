fad54aef8034dd8cad8c4f8133a00fab044bb11d
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2023-10-11 01:37:45 +0000

    Update and rename extensions.js to lockscreen@naterussell/extensions.js

---
+++ lockscreen@naterussell/extensions.js
@@ -0,0 +1,35 @@
+// Import necessary modules
+import { St, Clutter } from 'imports.gi';
+import { Main, Util } from 'imports.misc';
+
+let _lockScreenButton = null;
+
+function init() {
+    _lockScreenButton = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        y_align: Clutter.ActorAlign.CENTER,
+        track_hover: true
+    });
+
+    let icon = new St.Icon({
+        icon_name: 'changes-prevent-symbolic',
+        style_class: 'system-status-icon'
+    });
+
+    _lockScreenButton.set_child(icon);
+    _lockScreenButton.connect('button-press-event', _lockScreenActivate);
+}
+
+function _lockScreenActivate() {
+    Util.spawn(['/bin/bash', '-c', 'xscreensaver-command -lock']);
+}
+
+function enable() {
+    Main.panel._rightBox.insert_child_at_index(_lockScreenButton, 0);
+}
+
+function disable() {
+    Main.panel._rightBox.remove_actor(_lockScreenButton);
+}
