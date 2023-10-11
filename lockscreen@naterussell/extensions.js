fe033b1ac2ff03ee3cfa2bc570807305e14cfc2e
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2023-10-11 22:38:55 +0000

    Update extensions.js

--- lockscreen@naterussell/extensions.js
+++ lockscreen@naterussell/extensions.js
@@ -19,40 +19,17 @@
 import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
-import Clutter from 'gi://Clutter';
 
-import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import { Extension, Main, PanelMenu } from 'extensionSystem';
 
-// Main panel menu for the extension
 let lockScreenButton;
+let daemonProcess;
 
-let daemonProcess; // Variable to store the daemon process
+function startDaemonMode() {
+  daemonProcess = Gio.Subprocess.new(['xscreensaver-command', '-watch'], Gio.SubprocessFlags.STDOUT_PIPE);
+  daemonProcess.init(null);
+}
 
-// LockScreenButton class
-const LockScreenButton = GObject.registerClass(
-class LockScreenButton extends PanelMenu.Button {
-  _init() {
-    super._init(0);
-
-    let label = new St.Label({
-      text: "Lock Screen",
-      y_align: Clutter.ActorAlign.CENTER,
-    });
-
-    this.add_child(label);
-
-    this.connect('button-press-event', this._lockScreenActivate);
-  }
-
-  _lockScreenActivate() {
-    lockScreen(); // Call the lockScreen function to lock the screen
-  }
-});
-
-// Function to lock the screen using xscreensaver-command
 function lockScreen() {
   let [success, pid] = Gio.Subprocess.new(['xscreensaver-command', '-lock'], Gio.SubprocessFlags.NONE);
   if (success) {
@@ -60,51 +37,27 @@ function lockScreen() {
   }
 }
 
-// Function to start xscreensaver-command in daemon mode
-function startDaemonMode() {
-  daemonProcess = Gio.Subprocess.new(['xscreensaver-command', '-watch'], Gio.SubprocessFlags.STDOUT_PIPE);
-  daemonProcess.init(null);
-}
-
-// Function to stop the daemon process
-function stopDaemonMode() {
-  if (daemonProcess) {
-    daemonProcess.force_exit(); // Stop the daemon
-    daemonProcess = null; // Clear the reference
-  }
-}
-
-// Main extension class
 export default class LockScreenPopupExtension extends Extension {
   enable() {
-    this._lockScreenButton = new LockScreenButton();
-    Main.panel.addToStatusArea(this.metadata.uuid, this._lockScreenButton);
+    lockScreenButton = new PanelMenu.Button(0.0, "Lock Screen");
+    let button = new St.Button();
+    button.set_child(lockScreenButton);
+    lockScreenButton.add_child(button);
 
-    // Start xscreensaver-command in daemon mode when the extension is enabled
     startDaemonMode();
 
-    // Listen for lock and unlock signals (if required)
-    this._lockScreenSignalId = global.display.connect('lock-screen', () => {
-      this._lockScreenButton.visible = false;
+    button.connect('clicked', () => {
+      lockScreen();
     });
 
-    this._unlockScreenSignalId = global.display.connect('unlock-screen', () => {
-      this._lockScreenButton.visible = true;
-    });
+    Main.panel.addToStatusArea(this.metadata.uuid, lockScreenButton);
   }
 
   disable() {
-    // Stop the daemon mode when the extension is disabled
-    stopDaemonMode();
-
-    // Disconnect signals and destroy the button (if required)
-    if (this._lockScreenSignalId) {
-      global.display.disconnect(this._lockScreenSignalId);
+    if (daemonProcess) {
+      daemonProcess.force_exit();
+      daemonProcess = null;
     }
-    if (this._unlockScreenSignalId) {
-      global.display.disconnect(this._unlockScreenSignalId);
-    }
-    this._lockScreenButton.destroy();
-    this._lockScreenButton = null;
+    lockScreenButton.destroy();
   }
 }
